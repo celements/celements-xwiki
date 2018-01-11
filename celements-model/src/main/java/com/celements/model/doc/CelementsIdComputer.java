68c5988ee3c8ae747c6b412b1e122a54dc33b5fc
Marc Sladek <marc.sladek@synventis.com>
2018-01-11 12:56:14 +0100

    redesign CelementsIdComputer

--- celements-model/src/main/java/com/celements/model/doc/CelementsIdComputer.java
+++ celements-model/src/main/java/com/celements/model/doc/CelementsIdComputer.java
@@ -1,30 +1,52 @@
 package com.celements.model.doc;
 
+import javax.annotation.Nullable;
 import javax.validation.constraints.NotNull;
 
 import org.xwiki.component.annotation.ComponentRole;
 import org.xwiki.model.reference.DocumentReference;
 
+import com.xpn.xwiki.doc.XWikiDocument;
+
 @ComponentRole
 public interface CelementsIdComputer {
 
   /**
-   * @return computes the id for the given document
-   * @throws DocumentIdComputationException
+   * @return computes the id for the given document and language
+   * @throws IdComputationException
    *           if unable to compute an id
    */
-  long computeId(@NotNull DocumentReference docRef, @NotNull String lang)
-      throws DocumentIdComputationException;
+  long computeDocumentId(@NotNull DocumentReference docRef, @Nullable String lang)
+      throws IdComputationException;
 
-  public class DocumentIdComputationException extends Exception {
+  /**
+   * @return computes the id for the given document, language and collision count
+   * @throws IdComputationException
+   *           if unable to compute an id
+   */
+  long computeDocumentId(@NotNull DocumentReference docRef, @Nullable String lang,
+      int collisionCount) throws IdComputationException;
+
+  /**
+   * @return computes the id for the given document
+   * @throws IdComputationException
+   *           if unable to compute an id
+   */
+  long computeDocumentId(@NotNull XWikiDocument doc) throws IdComputationException;
+
+  public class IdComputationException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
-    protected DocumentIdComputationException(String message) {
+    protected IdComputationException(String message) {
       super(message);
     }
 
-    protected DocumentIdComputationException(String message, Throwable cause) {
+    protected IdComputationException(Throwable cause) {
+      super(cause);
+    }
+
+    protected IdComputationException(String message, Throwable cause) {
       super(message, cause);
     }
 
--- celements-model/src/main/java/com/celements/model/doc/UniqueHashIdComputer.java
+++ celements-model/src/main/java/com/celements/model/doc/UniqueHashIdComputer.java
@@ -1,5 +1,7 @@
 package com.celements.model.doc;
 
+import static com.google.common.base.Preconditions.*;
+
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -8,11 +10,12 @@ import org.xwiki.component.annotation.Component;
 import org.xwiki.component.annotation.Requirement;
 import org.xwiki.model.reference.DocumentReference;
 
-import com.celements.model.context.ModelContext;
 import com.celements.model.util.ModelUtils;
 import com.google.common.base.Splitter;
+import com.google.common.base.Strings;
+import com.google.common.base.Verify;
+import com.google.common.base.VerifyException;
 import com.google.common.primitives.Longs;
-import com.xpn.xwiki.XWikiException;
 import com.xpn.xwiki.doc.XWikiDocument;
 
 @Component(UniqueHashIdComputer.NAME)
@@ -21,56 +24,77 @@ public class UniqueHashIdComputer implements CelementsIdComputer {
   public static final String NAME = "uniqueHash";
 
   private static final String HASH_ALGO = "MD5";
-  private static final int BITS_COLLISION = 2;
 
   @Requirement
   private ModelUtils modelUtils;
 
-  @Requirement
-  private ModelContext context;
+  @Override
+  public long computeDocumentId(DocumentReference docRef, String lang)
+      throws IdComputationException {
+    return computeDocumentId(docRef, lang, 0);
+  }
 
   @Override
-  public long computeId(DocumentReference docRef, String lang)
-      throws DocumentIdComputationException {
-    long id = hashMD5(getLocalKey(docRef, lang)) << (BITS_COLLISION + getOffsetBits());
-    int collisionCount = 0;
-    while (isIdClaimed(docRef, lang, id)) {
-      if (collisionCount++ < (1 << BITS_COLLISION)) {
-        id = incrementId(id);
-      } else {
-        throw new DocumentIdComputationException("too many id collisions occured on id '" + id
-            + "' for: " + serialize(docRef, lang));
-      }
-    }
-    return id;
+  public long computeDocumentId(DocumentReference docRef, String lang, int collisionCount)
+      throws IdComputationException {
+    verifyCollisionCount(collisionCount);
+    long docId = hashMD5(serializeLocalUid(docRef, lang)) << getObjectBits();
+    return combine(docId, collisionCount, getCollisionBits());
+  }
+
+  @Override
+  public long computeDocumentId(XWikiDocument doc) throws IdComputationException {
+    checkNotNull(doc);
+    return computeDocumentId(doc.getDocumentReference(), doc.getLanguage());
   }
 
   /**
-   * @return true if ID is already claimed by another document, hence a hash collision happened
+   * bitwise combination of the long 'base' with 'bits' bits of the long 'msb' placed at the
+   * left (most significant bits)
    */
-  private boolean isIdClaimed(DocumentReference docRef, String lang, long id)
-      throws DocumentIdComputationException {
+  private long combine(long base, long msb, int bits) {
+    base = ~(~(base << bits) >>> bits); // set msb's to 1 for and'ing
+    msb = ~(~msb << (64 - bits)); // shift to left and set right side to 1 for and'ing
+    return base & msb;
+  }
+
+  /**
+   * @return first 8 bytes of MD5 hash from given string
+   */
+  private long hashMD5(String str) throws IdComputationException {
     try {
-      XWikiDocument doc = new CelementsDocument(docRef);
-      doc.setLanguage(lang);
-      doc.setId(id);
-      // use #loadXWikiDoc since #exists checks FN and not ID
-      doc = context.getXWikiContext().getWiki().getStore().loadXWikiDoc(doc,
-          context.getXWikiContext());
-      boolean isSameDoc = docRef.equals(doc.getDocumentReference()) && lang.equals(
-          doc.getLanguage());
-      // TODO warn log if not same
-      return !isSameDoc;
-    } catch (XWikiException exc) {
-      throw new DocumentIdComputationException("unable do load document: " + serialize(docRef,
-          lang), exc);
+      MessageDigest digest = MessageDigest.getInstance(HASH_ALGO);
+      digest.update(str.getBytes("utf-8"));
+      return Longs.fromByteArray(digest.digest());
+    } catch (NoSuchAlgorithmException | UnsupportedEncodingException exc) {
+      throw new IdComputationException("failed calculating hash", exc);
     }
   }
 
+  private void verifyCollisionCount(int collisionCount) throws IdComputationException {
+    try {
+      Verify.verify(collisionCount >= 0, "negative collision count '%s' not allowed",
+          collisionCount);
+      Verify.verify(collisionCount < (1 << getCollisionBits()),
+          "collision count '%s' outside of defined range '2^%s'", collisionCount,
+          getCollisionBits());
+    } catch (VerifyException exc) {
+      throw new IdComputationException(exc);
+    }
+  }
+
+  private int getCollisionBits() {
+    return 2; // TODO configurable?
+  }
+
+  private int getObjectBits() {
+    return 12; // TODO configurable?
+  }
+
   /**
-   * @return calculated local key like LocalUidStringEntityReferenceSerializer from XWiki 4.0+
+   * @return calculated local uid like LocalUidStringEntityReferenceSerializer from XWiki 4.0+
    */
-  private String getLocalKey(DocumentReference docRef, String lang) {
+  private String serializeLocalUid(DocumentReference docRef, String lang) {
     StringBuilder key = new StringBuilder();
     for (String name : Splitter.on('.').split(serialize(docRef, lang))) {
       if (!name.isEmpty()) {
@@ -80,30 +104,8 @@ public class UniqueHashIdComputer implements CelementsIdComputer {
     return key.toString();
   }
 
-  /**
-   * @return first 8 bytes of MD5 hash from given string
-   */
-  private long hashMD5(String str) throws DocumentIdComputationException {
-    try {
-      MessageDigest digest = MessageDigest.getInstance(HASH_ALGO);
-      digest.update(str.getBytes("utf-8"));
-      return Longs.fromByteArray(digest.digest());
-    } catch (NoSuchAlgorithmException | UnsupportedEncodingException exc) {
-      throw new DocumentIdComputationException("failed calculating hash", exc);
-    }
-  }
-
-  private long incrementId(long id) {
-    // increment at offset defined position
-    return ((id >> getOffsetBits()) + 1) << getOffsetBits();
-  }
-
-  private int getOffsetBits() {
-    return CelementsDocument.BITS_OBJECT_OFFSET;
-  }
-
   private String serialize(DocumentReference docRef, String lang) {
-    return modelUtils.serializeRefLocal(docRef) + '.' + lang;
+    return modelUtils.serializeRefLocal(docRef) + '.' + Strings.nullToEmpty(lang).trim();
   }
 
 }
