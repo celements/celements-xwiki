09cce6d2b863dd34fe51bc59804d82bfac9abcb0
Marc Sladek <marc.sladek@synventis.com>
2018-01-26 01:11:19 +0100

    add XWikiDocument#hasValidId

--- celements-xwiki-core/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
+++ celements-xwiki-core/src/main/java/com/xpn/xwiki/doc/XWikiDocument.java
@@ -563,11 +563,19 @@ public class XWikiDocument implements DocumentModelBridge
         this.id = id;
     }
 
+    public boolean hasValidId() {
+      return idVersion != null;
+    }
+
     public IdVersion getIdVersion() {
-      if (idVersion == null) {
-        throw new IllegalStateException("no version set");
-      }
+      verifyIdVersion();
       return idVersion;
+    }
+    
+    private void verifyIdVersion() {
+      if (!hasValidId()) {
+        throw new IllegalStateException("no id version set");
+      }
     }
 
     /**
