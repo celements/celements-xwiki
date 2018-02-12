276ecbafe85b360d71a9714ff19ef449bb5b41a7
Marc Sladek <marc.sladek@synventis.com>
2018-02-13 00:58:29 +0100

    BaseCollectionIdMigration drop/add FK

--- celements-model/src/main/java/com/celements/model/migration/BaseCollectionIdMigration.java
+++ celements-model/src/main/java/com/celements/model/migration/BaseCollectionIdMigration.java
@@ -1,7 +1,14 @@
 package com.celements.model.migration;
 
+import static com.google.common.base.Preconditions.*;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.mutabilitydetector.internal.com.google.common.base.Joiner;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.xwiki.component.annotation.Component;
@@ -10,6 +17,8 @@ import org.xwiki.component.annotation.Requirement;
 import com.celements.migrations.SubSystemHibernateMigrationManager;
 import com.celements.migrator.AbstractCelementsHibernateMigrator;
 import com.celements.query.IQueryExecutionServiceRole;
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableMap.Builder;
 import com.xpn.xwiki.XWikiContext;
@@ -23,7 +32,9 @@ public class BaseCollectionIdMigration extends AbstractCelementsHibernateMigrato
 
   public static final String NAME = "BaseCollectionIdMigration";
 
-  private static final Map<String, String> TABLE_COL_MAP;
+  private static final List<String> TABLES = ImmutableList.of("xwikiclasses", "xwikiclassesprop",
+      "xwikiobjects", "xwikiproperties", "xwikistatsdoc", "xwikistatsreferer", "xwikistatsvisit");
+  private static final Map<String, String> TABLE_ID_MAP;
 
   static {
     Builder<String, String> builder = ImmutableMap.builder();
@@ -34,7 +45,8 @@ public class BaseCollectionIdMigration extends AbstractCelementsHibernateMigrato
     builder.put("xwikistatsdoc", "XWS_ID");
     builder.put("xwikistatsreferer", "XWR_ID");
     builder.put("xwikistatsvisit", "XWV_ID");
-    TABLE_COL_MAP = builder.build();
+    TABLE_ID_MAP = builder.build();
+    checkState(TABLE_ID_MAP.keySet().containsAll(TABLES));
   }
 
   @Requirement
@@ -52,25 +64,100 @@ public class BaseCollectionIdMigration extends AbstractCelementsHibernateMigrato
 
   /**
    * getVersion is using days since 1.1.2010 until the day of committing this migration
-   * 08.02.2018 -> 2960 http://www.wolframalpha.com/input/?i=days+since+01.01.2010
+   * 13.02.2018 -> 2965 http://www.wolframalpha.com/input/?i=days+since+01.01.2010
    */
   @Override
   public XWikiDBVersion getVersion() {
-    return new XWikiDBVersion(2960);
+    return new XWikiDBVersion(2965);
   }
 
   @Override
   public void migrate(SubSystemHibernateMigrationManager manager, XWikiContext context)
       throws XWikiException {
-    for (String table : TABLE_COL_MAP.keySet()) {
-      String column = TABLE_COL_MAP.get(table);
-      int resultCount = queryExecutor.executeWriteSQL(createAlterSql(table, column));
-      LOGGER.info("altered table {} with {} entries to bigint", table, resultCount);
+    for (String table : TABLES) {
+      LOGGER.info("migrating id for {}", table);
+      Collection<ForeignKey> foreignKeys = loadForeignKeys(table, context.getDatabase());
+      for (ForeignKey fk : foreignKeys) {
+        LOGGER.debug("adding {}", fk);
+        queryExecutor.executeWriteSQL(fk.getDropForeignKeySql());
+      }
+      int count = queryExecutor.executeWriteSQL(getModifyIdSql(table));
+      LOGGER.info("updated {} id for {} rows", table, count);
+      for (ForeignKey fk : foreignKeys) {
+        LOGGER.debug("dropping {}", fk);
+        queryExecutor.executeWriteSQL(fk.getAddForeignKeySql());
+      }
     }
   }
 
-  String createAlterSql(String table, String column) {
-    return "alter table " + table + " modify column " + column + " bigint not null";
+  String getModifyIdSql(String table) {
+    return "alter table " + table + " modify column " + TABLE_ID_MAP.get(table)
+        + " bigint not null";
+  }
+
+  private Collection<ForeignKey> loadForeignKeys(String table, String database)
+      throws XWikiException {
+    String sql = getLoadForeignKeysSql(table, database);
+    Map<String, ForeignKey> map = new HashMap<>();
+    for (List<String> row : queryExecutor.executeReadSql(String.class, sql)) {
+      String name = row.get(0);
+      ForeignKey fk = map.get(name);
+      if (fk == null) {
+        map.put(name, fk = new ForeignKey(name, row.get(1), table));
+      }
+      fk.addColumn(row.get(2), row.get(3));
+    }
+    return map.values();
+  }
+
+  String getLoadForeignKeysSql(String table, String database) {
+    return "select CONSTRAINT_NAME, TABLE_NAME, COLUMN_NAME, REFERENCED_COLUMN_NAME "
+        + "from INFORMATION_SCHEMA.KEY_COLUMN_USAGE where TABLE_SCHEMA = '" + database
+        + "' and REFERENCED_TABLE_NAME = '" + table
+        + "' order by CONSTRAINT_NAME, ORDINAL_POSITION";
+  }
+
+  class ForeignKey {
+
+    private final String name;
+    private final String table;
+    private final String referencedTable;
+    private final List<String> columns = new ArrayList<>();
+    private final List<String> referencedColumns = new ArrayList<>();
+
+    ForeignKey(String name, String table, String referencedTable) {
+      this.name = validate(name);
+      this.table = validate(table);
+      this.referencedTable = validate(referencedTable);
+    }
+
+    public ForeignKey addColumn(String column, String referencedColumn) {
+      columns.add(validate(column));
+      referencedColumns.add(validate(referencedColumn));
+      return this;
+    }
+
+    public String getAddForeignKeySql() {
+      return "alter table " + table + " add constraint " + name + " foreign key (" + Joiner.on(
+          ',').join(columns) + ")" + " references " + referencedTable + " (" + Joiner.on(',').join(
+              referencedColumns) + ")";
+    }
+
+    public String getDropForeignKeySql() {
+      return "alter table " + table + " drop foreign key " + name;
+    }
+
+    private String validate(String str) {
+      return checkNotNull(Strings.emptyToNull(str));
+    }
+
+    @Override
+    public String toString() {
+      return "ForeignKey [name=" + name + ", table=" + table + ", referencedTable="
+          + referencedTable + ", columns=" + columns + ", referencedColumns=" + referencedColumns
+          + "]";
+    }
+
   }
 
 }
--- celements-model/src/test/java/com/celements/model/migration/BaseCollectionIdMigrationTest.java
+++ celements-model/src/test/java/com/celements/model/migration/BaseCollectionIdMigrationTest.java
@@ -4,6 +4,10 @@ import static com.celements.common.test.CelementsTestUtils.*;
 import static org.easymock.EasyMock.*;
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,27 +31,58 @@ public class BaseCollectionIdMigrationTest extends AbstractComponentTest {
 
   @Test
   public void test_migrate() throws Exception {
-    expectSql("xwikiclasses", "XWO_ID");
-    expectSql("xwikiclassesprop", "XWP_ID");
-    expectSql("xwikiobjects", "XWO_ID");
-    expectSql("xwikiproperties", "XWP_ID");
-    expectSql("xwikistatsdoc", "XWS_ID");
-    expectSql("xwikistatsreferer", "XWR_ID");
-    expectSql("xwikistatsvisit", "XWV_ID");
+    expectModify("xwikiclasses");
+    expectModify("xwikiclassesprop");
+    expectModify("xwikiobjects");
+    expectModifyWithFk("xwikiproperties");
+    expectModify("xwikistatsdoc");
+    expectModify("xwikistatsreferer");
+    expectModify("xwikistatsvisit");
     replayDefault();
-    migration.migrate(null, null);
+    migration.migrate(null, getContext());
     verifyDefault();
   }
 
-  private void expectSql(String table, String column) throws XWikiException {
-    expect(queryExecMock.executeWriteSQL(migration.createAlterSql(table, column))).andReturn(
-        1).once();
+  private void expectModify(String table) throws XWikiException {
+    List<List<String>> fks = new ArrayList<>();
+    expect(queryExecMock.executeReadSql(String.class, migration.getLoadForeignKeysSql(table,
+        getContext().getDatabase()))).andReturn(fks).once();
+    expect(queryExecMock.executeWriteSQL(migration.getModifyIdSql(table))).andReturn(1).once();
+  }
+
+  private void expectModifyWithFk(String table) throws XWikiException {
+    List<List<String>> fks = new ArrayList<>();
+    fks.add(Arrays.asList("FK2780715A3433FD87", "xwikistrings", "XWS_ID", "XWP_ID"));
+    fks.add(Arrays.asList("FK2780715A3433FD87", "xwikistrings", "XWS_NAME", "XWP_NAME"));
+    fks.add(Arrays.asList("FKDF25AE4F283EE295", "xwikilongs", "XWL_ID", "XWP_ID"));
+    fks.add(Arrays.asList("FKDF25AE4F283EE295", "xwikilongs", "XWL_NAME", "XWP_NAME"));
+    expect(queryExecMock.executeReadSql(String.class, migration.getLoadForeignKeysSql(table,
+        getContext().getDatabase()))).andReturn(fks).once();
+    expect(queryExecMock.executeWriteSQL("alter table xwikistrings "
+        + "drop foreign key FK2780715A3433FD87")).andReturn(1).once();
+    expect(queryExecMock.executeWriteSQL("alter table xwikilongs "
+        + "drop foreign key FKDF25AE4F283EE295")).andReturn(1).once();
+    expect(queryExecMock.executeWriteSQL(migration.getModifyIdSql(table))).andReturn(1).once();
+    expect(queryExecMock.executeWriteSQL("alter table xwikistrings "
+        + "add constraint FK2780715A3433FD87 foreign key (XWS_ID,XWS_NAME) references " + table
+        + " (XWP_ID,XWP_NAME)")).andReturn(1).once();
+    expect(queryExecMock.executeWriteSQL("alter table xwikilongs "
+        + "add constraint FKDF25AE4F283EE295 foreign key (XWL_ID,XWL_NAME) references " + table
+        + " (XWP_ID,XWP_NAME)")).andReturn(1).once();
   }
 
   @Test
-  public void test_createAlterSql() throws Exception {
-    assertEquals("alter table tbl modify column col bigint not null", migration.createAlterSql(
-        "tbl", "col"));
+  public void test_getModifyIdSql() throws Exception {
+    assertEquals("alter table xwikiobjects modify column XWO_ID bigint not null",
+        migration.getModifyIdSql("xwikiobjects"));
+  }
+
+  @Test
+  public void test_getLoadForeignKeysSql() throws Exception {
+    assertEquals("select CONSTRAINT_NAME, TABLE_NAME, COLUMN_NAME, REFERENCED_COLUMN_NAME "
+        + "from INFORMATION_SCHEMA.KEY_COLUMN_USAGE where TABLE_SCHEMA = 'db' and "
+        + "REFERENCED_TABLE_NAME = 'xwikiproperties' order by CONSTRAINT_NAME, ORDINAL_POSITION",
+        migration.getLoadForeignKeysSql("xwikiproperties", "db"));
   }
 
 }
