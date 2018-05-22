255c754099dcaf4f371e5a2a0cb44408795aabbb
Marc Sladek <marc.sladek@synventis.com>
2018-05-22 19:06:22 +0200

    change XWikiGroups to DocRef

--- celements-model/src/main/java/com/celements/web/classes/oldcore/XWikiGroupsClass.java
+++ celements-model/src/main/java/com/celements/web/classes/oldcore/XWikiGroupsClass.java
@@ -3,11 +3,11 @@ package com.celements.web.classes.oldcore;
 import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
+import org.xwiki.model.reference.DocumentReference;
 
 import com.celements.model.classes.AbstractClassDefinition;
 import com.celements.model.classes.fields.ClassField;
-import com.celements.model.classes.fields.UserField;
-import com.xpn.xwiki.user.api.XWikiUser;
+import com.celements.model.classes.fields.ref.DocumentReferenceField;
 
 @Singleton
 @Component(XWikiGroupsClass.CLASS_DEF_HINT)
@@ -17,8 +17,8 @@ public class XWikiGroupsClass extends AbstractClassDefinition implements IOldCor
   public static final String CLASS_FN = CLASS_SPACE + "." + CLASS_NAME;
   public static final String CLASS_DEF_HINT = CLASS_FN;
 
-  public static final ClassField<XWikiUser> FIELD_MEMBER = new UserField.Builder(CLASS_FN,
-      "member").prettyName("Member").size(30).build();
+  public static final ClassField<DocumentReference> FIELD_MEMBER = new DocumentReferenceField.Builder(
+      CLASS_FN, "member").prettyName("Member").size(30).build();
 
   @Override
   public String getName() {
