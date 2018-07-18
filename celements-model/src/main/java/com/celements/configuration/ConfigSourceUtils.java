b8ac74fbeeb723fb99a704e3671b4691ee9d8a24
Marc Sladek <marc.sladek@synventis.com>
2018-07-18 18:38:17 +0200

    ConfigSourceUtils extensions (#64)

--- celements-model/src/main/java/com/celements/configuration/ConfigSourceUtils.java
+++ celements-model/src/main/java/com/celements/configuration/ConfigSourceUtils.java
@@ -1,22 +1,45 @@
 package com.celements.configuration;
 
+import static com.google.common.base.Strings.*;
+
 import java.util.List;
 
 import javax.validation.constraints.NotNull;
 
 import org.xwiki.configuration.ConfigurationSource;
+import org.xwiki.model.reference.EntityReference;
 
 import com.celements.common.MorePredicates;
+import com.celements.model.util.ModelUtils;
 import com.google.common.base.Functions;
+import com.google.common.base.Optional;
 import com.google.common.base.Predicates;
 import com.google.common.collect.FluentIterable;
+import com.xpn.xwiki.web.Utils;
 
 public class ConfigSourceUtils {
 
   private ConfigSourceUtils() {
   }
 
-  public static @NotNull List<String> getStringListProperty(@NotNull ConfigurationSource configSrc,
+  @NotNull
+  public static Optional<String> getStringProperty(@NotNull String key) {
+    return getStringProperty(getDefaultCfgSrc(), key);
+  }
+
+  @NotNull
+  public static Optional<String> getStringProperty(@NotNull ConfigurationSource configSrc,
+      @NotNull String key) {
+    return Optional.fromNullable(emptyToNull(configSrc.getProperty(key, "").trim()));
+  }
+
+  @NotNull
+  public static List<String> getStringListProperty(@NotNull String key) {
+    return getStringListProperty(getDefaultCfgSrc(), key);
+  }
+
+  @NotNull
+  public static List<String> getStringListProperty(@NotNull ConfigurationSource configSrc,
       @NotNull String key) {
     FluentIterable<?> values;
     Object prop = configSrc.getProperty(key);
@@ -29,6 +52,30 @@ public class ConfigSourceUtils {
     }
     return values.filter(Predicates.notNull()).transform(Functions.toStringFunction()).filter(
         MorePredicates.stringNotBlankPredicate()).toList();
+  }
+
+  @NotNull
+  public static <T extends EntityReference> Optional<T> getReferenceProperty(@NotNull String key,
+      @NotNull Class<T> token) {
+    return getReferenceProperty(getDefaultCfgSrc(), key, token);
+  }
+
+  @NotNull
+  public static <T extends EntityReference> Optional<T> getReferenceProperty(
+      @NotNull ConfigurationSource configSrc, @NotNull String key, @NotNull Class<T> token) {
+    try {
+      return Optional.of(getModelUtils().resolveRef(configSrc.getProperty(key, ""), token));
+    } catch (IllegalArgumentException iae) {
+      return Optional.absent();
+    }
+  }
+
+  private static ModelUtils getModelUtils() {
+    return Utils.getComponent(ModelUtils.class);
+  }
+
+  private static ConfigurationSource getDefaultCfgSrc() {
+    return Utils.getComponent(ConfigurationSource.class);
   }
 
 }
