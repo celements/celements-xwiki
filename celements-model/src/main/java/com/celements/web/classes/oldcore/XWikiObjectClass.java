633b12dd42e56ec1e49b780c7ea274e89b02f5ec
Marc Sladek <marc.sladek@synventis.com>
2018-12-04 01:44:46 +0100

    add XWikiObjectClass.FIELD_DOC_REF

--- celements-model/src/main/java/com/celements/web/classes/oldcore/XWikiObjectClass.java
+++ celements-model/src/main/java/com/celements/web/classes/oldcore/XWikiObjectClass.java
@@ -4,11 +4,13 @@ import javax.inject.Singleton;
 
 import org.xwiki.component.annotation.Component;
 import org.xwiki.model.reference.ClassReference;
+import org.xwiki.model.reference.DocumentReference;
 
 import com.celements.model.classes.PseudoClassDefinition;
 import com.celements.model.classes.fields.ClassField;
 import com.celements.model.classes.fields.number.IntField;
 import com.celements.model.classes.fields.ref.ClassReferenceField;
+import com.celements.model.classes.fields.ref.DocumentReferenceField;
 
 @Singleton
 @Component(XWikiObjectClass.CLASS_DEF_HINT)
@@ -17,6 +19,9 @@ public class XWikiObjectClass extends PseudoClassDefinition {
   public static final String CLASS_NAME = "XWikiObjectClass";
   public static final String CLASS_FN = CLASS_SPACE + "." + CLASS_NAME;
   public static final String CLASS_DEF_HINT = CLASS_FN;
+
+  public static final ClassField<DocumentReference> FIELD_DOC_REF = new DocumentReferenceField.Builder(
+      CLASS_FN, "documentReference").build();
 
   public static final ClassField<ClassReference> FIELD_CLASS_REF = new ClassReferenceField.Builder(
       CLASS_FN, "classReference").build();
