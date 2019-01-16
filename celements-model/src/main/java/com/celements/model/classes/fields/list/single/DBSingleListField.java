bd394e89d27ff075a4ad87d3615e167778c2bebd
Marc Sladek <marc.sladek@synventis.com>
2019-01-16 21:22:02 +0100

    add CustomDBSingleListField

--- celements-model/src/main/java/com/celements/model/classes/fields/list/single/DBSingleListField.java
+++ celements-model/src/main/java/com/celements/model/classes/fields/list/single/DBSingleListField.java
@@ -1,21 +1,14 @@
 package com.celements.model.classes.fields.list.single;
 
-import javax.annotation.Nullable;
 import javax.annotation.concurrent.Immutable;
 import javax.validation.constraints.NotNull;
 
 import com.celements.marshalling.DefaultMarshaller;
-import com.xpn.xwiki.objects.classes.DBListClass;
-import com.xpn.xwiki.objects.classes.ListClass;
 
 @Immutable
-public final class DBSingleListField extends SingleListField<String> {
+public final class DBSingleListField extends CustomDBSingleListField<String> {
 
-  private final String sql;
-
-  public static class Builder extends SingleListField.Builder<Builder, String> {
-
-    private String sql;
+  public final static class Builder extends CustomDBSingleListField.Builder<Builder, String> {
 
     public Builder(@NotNull String classDefName, @NotNull String name) {
       super(classDefName, name, new DefaultMarshaller());
@@ -24,11 +17,6 @@ public final class DBSingleListField extends SingleListField<String> {
     @Override
     public Builder getThis() {
       return this;
-    }
-
-    public Builder sql(@Nullable String val) {
-      sql = val;
-      return getThis();
     }
 
     @Override
@@ -40,20 +28,6 @@ public final class DBSingleListField extends SingleListField<String> {
 
   protected DBSingleListField(@NotNull Builder builder) {
     super(builder);
-    this.sql = builder.sql;
-  }
-
-  public String getSql() {
-    return sql;
-  }
-
-  @Override
-  protected ListClass getListClass() {
-    DBListClass element = new DBListClass();
-    if (sql != null) {
-      element.setSql(sql);
-    }
-    return element;
   }
 
 }
