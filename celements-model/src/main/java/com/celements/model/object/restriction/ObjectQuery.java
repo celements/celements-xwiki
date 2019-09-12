c1d1973dbcb36e9cebbc9980d297978ffab64db0
Marc Sladek <marc.sladek@synventis.com>
2019-09-12 22:29:06 +0200

    generalise ObjectRestriction with Predicate<BaseObject>

--- celements-model/src/main/java/com/celements/model/object/restriction/ObjectQuery.java
+++ celements-model/src/main/java/com/celements/model/object/restriction/ObjectQuery.java
@@ -3,53 +3,52 @@ package com.celements.model.object.restriction;
 import java.util.LinkedHashSet;
 import java.util.Objects;
 import java.util.Set;
+import java.util.function.Predicate;
 
 import javax.annotation.concurrent.NotThreadSafe;
 
 import com.celements.model.classes.ClassIdentity;
-import com.google.common.base.Function;
-import com.google.common.base.Predicate;
+import com.google.common.base.Predicates;
 import com.google.common.collect.FluentIterable;
 
 @NotThreadSafe
 public class ObjectQuery<O> {
 
-  private Set<ObjectRestriction<O>> restrictions = new LinkedHashSet<>();
+  private Set<Predicate<O>> restrictions = new LinkedHashSet<>();
 
   public ObjectQuery() {
   }
 
-  public ObjectQuery(Iterable<? extends ObjectRestriction<O>> iter) {
+  public ObjectQuery(Iterable<? extends Predicate<O>> iter) {
     this.addAll(iter);
   }
 
-  public void add(ObjectRestriction<O> restr) {
+  public void add(Predicate<O> restr) {
     restrictions.add(restr);
   }
 
-  public void addAll(Iterable<? extends ObjectRestriction<O>> iter) {
-    for (ObjectRestriction<O> restr : iter) {
+  public void addAll(Iterable<? extends Predicate<O>> iter) {
+    for (Predicate<O> restr : iter) {
       this.add(restr);
     }
   }
 
-  public FluentIterable<ObjectRestriction<O>> getRestrictions() {
+  public FluentIterable<Predicate<O>> getRestrictions() {
     return FluentIterable.from(restrictions);
   }
 
-  public FluentIterable<ObjectRestriction<O>> getRestrictions(ClassIdentity classId) {
-    return getRestrictions().filter(new ClassPredicate(classId));
+  public Predicate<O> predicate(ClassIdentity classId) {
+    return restrictions.stream()
+        .filter(new ClassPredicate(classId))
+        .reduce((p1, p2) -> p1.and(p2))
+        .orElse(Predicates.alwaysTrue());
   }
 
   public Set<ClassIdentity> getObjectClasses() {
-    return getRestrictions().filter(getClassRestrictionClass()).transform(
-        new Function<ClassRestriction<O>, ClassIdentity>() {
-
-          @Override
-          public ClassIdentity apply(ClassRestriction<O> restr) {
-            return restr.getClassIdentity();
-          }
-        }).toSet();
+    return getRestrictions()
+        .filter(getClassRestrictionClass())
+        .transform(ClassRestriction::getClassIdentity)
+        .toSet();
   }
 
   @SuppressWarnings("unchecked")
@@ -85,7 +84,7 @@ public class ObjectQuery<O> {
     return "ObjectQuery " + restrictions;
   }
 
-  private class ClassPredicate implements Predicate<ObjectRestriction<?>> {
+  private class ClassPredicate implements com.google.common.base.Predicate<Predicate<?>> {
 
     private final ClassIdentity classId;
 
@@ -94,7 +93,7 @@ public class ObjectQuery<O> {
     }
 
     @Override
-    public boolean apply(ObjectRestriction<?> restr) {
+    public boolean apply(Predicate<?> restr) {
       if (restr instanceof ClassRestriction) {
         return ((ClassRestriction<?>) restr).getClassIdentity().equals(classId);
       }
