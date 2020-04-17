d516b98a1628c3ab15a46499c7925f286180845b
Marc Sladek <marc@sladek.me>
2020-04-17 03:36:19 +0200

    improve defaultValue

--- celements-model/src/main/java/com/celements/common/MoreObjectsCel.java
+++ celements-model/src/main/java/com/celements/common/MoreObjectsCel.java
@@ -1,17 +1,17 @@
 package com.celements.common;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.HashSet;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Stream;
 
 import com.google.common.base.Defaults;
-import com.google.common.collect.FluentIterable;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
@@ -36,20 +36,21 @@ public final class MoreObjectsCel {
 
   @SuppressWarnings("unchecked")
   private static <T> T defaultValue(Class<T> type, boolean mutable) {
-    if (Iterable.class.equals(type) || Collection.class.equals(type)
-        || List.class.equals(type)) {
+    if (List.class.isAssignableFrom(type)) {
       return (T) (mutable ? new ArrayList<>() : ImmutableList.of());
-    } else if (Set.class.equals(type)) {
-      return (T) (mutable ? new HashSet<>() : ImmutableSet.of());
-    } else if (Map.class.equals(type)) {
-      return (T) (mutable ? new HashMap<>() : ImmutableMap.of());
-    } else if (Stream.class.equals(type)) {
+    } else if (Set.class.isAssignableFrom(type)) {
+      return (T) (mutable ? new LinkedHashSet<>() : ImmutableSet.of());
+    } else if (Queue.class.isAssignableFrom(type)) {
+      return (T) new LinkedList<>();
+    } else if (Iterable.class.isAssignableFrom(type)) {
+      return (T) defaultValue(List.class, mutable);
+    } else if (Map.class.isAssignableFrom(type)) {
+      return (T) (mutable ? new LinkedHashMap<>() : ImmutableMap.of());
+    } else if (Stream.class.isAssignableFrom(type)) {
       return (T) Stream.empty();
-    } else if (FluentIterable.class.equals(type)) {
-      return (T) FluentIterable.of();
-    } else if (Optional.class.equals(type)) {
+    } else if (Optional.class.isAssignableFrom(type)) {
       return (T) Optional.empty();
-    } else if (com.google.common.base.Optional.class.equals(type)) {
+    } else if (com.google.common.base.Optional.class.isAssignableFrom(type)) {
       return (T) com.google.common.base.Optional.absent();
     } else {
       return Defaults.defaultValue(type);
