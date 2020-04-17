245050ec221adbcb49d06ecdfe43870f9727873c
Marc Sladek <marc@sladek.me>
2020-04-17 03:53:56 +0200

    improve defaultValue

--- celements-model/src/main/java/com/celements/common/MoreObjectsCel.java
+++ celements-model/src/main/java/com/celements/common/MoreObjectsCel.java
@@ -11,32 +11,44 @@ import java.util.Queue;
 import java.util.Set;
 import java.util.stream.Stream;
 
+import javax.annotation.Nullable;
+import javax.validation.constraints.NotNull;
+
 import com.google.common.base.Defaults;
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
+import com.google.common.primitives.Primitives;
 
 public final class MoreObjectsCel {
 
   private MoreObjectsCel() {}
 
-  public static <F, T> Optional<T> tryCast(F candidate, Class<T> targetClass) {
+  @NotNull
+  public static <F, T> Optional<T> tryCast(@Nullable F candidate, @NotNull Class<T> targetClass) {
     return targetClass.isInstance(candidate)
         ? Optional.of(targetClass.cast(candidate))
         : Optional.empty();
   }
 
-  public static <T> T defaultValue(Class<T> type) {
+  @Nullable
+  public static <T> T defaultValue(@NotNull Class<T> type) {
     return defaultValue(type, false);
   }
 
-  public static <T> T defaultMutableValue(Class<T> type) {
+  @Nullable
+  public static <T> T defaultMutableValue(@NotNull Class<T> type) {
     return defaultValue(type, true);
   }
 
   @SuppressWarnings("unchecked")
   private static <T> T defaultValue(Class<T> type, boolean mutable) {
-    if (List.class.isAssignableFrom(type)) {
+    T value = Defaults.defaultValue(Primitives.unwrap(type));
+    if (value != null) {
+      return value;
+    } else if (String.class.equals(type)) {
+      return (T) "";
+    } else if (List.class.isAssignableFrom(type)) {
       return (T) (mutable ? new ArrayList<>() : ImmutableList.of());
     } else if (Set.class.isAssignableFrom(type)) {
       return (T) (mutable ? new LinkedHashSet<>() : ImmutableSet.of());
@@ -53,7 +65,7 @@ public final class MoreObjectsCel {
     } else if (com.google.common.base.Optional.class.isAssignableFrom(type)) {
       return (T) com.google.common.base.Optional.absent();
     } else {
-      return Defaults.defaultValue(type);
+      return null;
     }
   }
 
