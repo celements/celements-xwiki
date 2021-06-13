18922a2ba2c0a164f3c220ada323325a9a288b9a
Marc Sladek <marc@sladek.me>
2021-06-13 18:07:58 +0200

    fix ForkFunktionTest

--- celements-model/src/test/java/com/celements/common/function/ForkFunctionTest.java
+++ celements-model/src/test/java/com/celements/common/function/ForkFunctionTest.java
@@ -1,14 +1,11 @@
 package com.celements.common.function;
 
-import static com.celements.common.lambda.LambdaExceptionUtil.*;
 import static org.junit.Assert.*;
 
-import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.List;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang.math.NumberUtils;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -27,7 +24,7 @@ public class ForkFunctionTest {
   public void test_when_then_else() {
     Stream<Number> numbers = inputNumbers.stream()
         .flatMap(new ForkFunction<String, Number>()
-            .when(NumberUtils::isDigits)
+            .when(this::isInt)
             .thenMap(Integer::parseInt)
             .elseMap(Double::parseDouble)
             .stream());
@@ -35,20 +32,20 @@ public class ForkFunctionTest {
   }
 
   @Test
-  public void test_noWhen() {
+  public void test_noWhen() throws ParseException {
     Stream<Number> numbers = inputNumbers.stream()
         .flatMap(new ForkFunction<String, Number>()
-            .thenMap(Integer::parseInt)
-            .elseMap(Double::parseDouble)
+            .thenMap(s -> 1)
+            .elseMap(s -> 0)
             .stream());
-    assertSum(10, numbers);
+    assertSum(inputNumbers.size(), numbers);
   }
 
   @Test
   public void test_noThen() {
     Stream<Number> numbers = inputNumbers.stream()
         .flatMap(new ForkFunction<String, Number>()
-            .when(NumberUtils::isDigits)
+            .when(this::isInt)
             .elseMap(Double::parseDouble)
             .stream());
     assertSum(6, numbers);
@@ -58,7 +55,7 @@ public class ForkFunctionTest {
   public void test_noElse() {
     Stream<Number> numbers = inputNumbers.stream()
         .flatMap(new ForkFunction<String, Number>()
-            .when(NumberUtils::isDigits)
+            .when(this::isInt)
             .thenMap(Integer::parseInt)
             .stream());
     assertSum(4, numbers);
@@ -66,18 +63,23 @@ public class ForkFunctionTest {
 
   @Test
   public void test_elseFilter() throws ParseException {
-    Stream<String> input = StreamEx.of(inputNumbers).zipWith("ACDC".chars(),
-        (letter, number) -> Stream.of(letter, number).map(Object::toString))
+    Stream<String> input = StreamEx.of(inputNumbers)
+        .filter(this::isInt)
+        .zipWith(Stream.of("W", "C"), (x, y) -> Stream.of(x, y))
         .flatMap(x -> x);
     StringBuilder letters = new StringBuilder();
     Stream<Number> numbers = input
         .flatMap(new ForkFunction<String, Number>()
-            .when(NumberUtils::isNumber)
-            .thenMap(rethrowFunction(NumberFormat.getInstance()::parse))
+            .when(this::isInt)
+            .thenMap(Integer::parseInt)
             .elseFilter(letters::append)
             .stream());
     assertSum(4, numbers);
-    assertEquals("ACDC", letters.toString());
+    assertEquals("WC", letters.toString());
+  }
+
+  private boolean isInt(String str) {
+    return str.matches("\\d+");
   }
 
   private void assertSum(double sum, Stream<Number> numbers) {
